0e60c63cb5fe0418e91ab2ebce2648193eea07e5
David Grier <david.grier@nyu.edu>
2021-08-07 08:33:11 -0400

    initial commit

---
+++ QSR830.py
@@ -0,0 +1,26 @@
+from QInstrument.lib import QInstrument
+from QInstrument import SR830
+
+
+class QSR830(QInstrument):
+    '''Stanford Research Systems SR830 Lockin Amplifier
+    '''
+    def __init__(self, **kwargs):
+        super().__init__(uiFile='SR830Widget.ui',
+                         deviceClass=SR830,
+                         **kwargs)
+
+
+def main():
+    import sys
+    from PyQt5.QtWidgets import QApplication
+
+    app = QApplication(sys.argv)
+    widget = QSR830()
+    widget.show()
+    sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    main()
+    
