1bf09057f5edce790712e2ef18a64ef60cd9862d
David Grier <david.grier@nyu.edu>
2021-08-07 11:00:11 -0400

    corrected __all__

--- __init__.py
+++ __init__.py
@@ -1,6 +1,7 @@
 from .SR830 import SR830
 from .DS345 import DS345
 from .QDS345 import QDS345
+from .QSR830 import QSR830
 
 
-__all__ = [DS345, SR830, QDS345]
+__all__ = ['DS345', 'SR830', 'QDS345', 'QSR830']
--- lib/__init__.py
+++ lib/__init__.py
@@ -2,5 +2,5 @@ from .QInstrument import QInstrument
 from .SerialInstrument import SerialInstrument
 
 
-__all__ = [SerialInstrument, QInstrument]
+__all__ = ['SerialInstrument', 'QInstrument']
 
