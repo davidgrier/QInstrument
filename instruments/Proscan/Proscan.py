738fac251da089fc6e5f4548e65de2b5a6d5bc5c
davidgrier <david.grier@nyu.edu>
2021-08-19 06:59:55 -0400

    implement motion controls

--- instruments/Proscan/Proscan.py
+++ instruments/Proscan/Proscan.py
@@ -33,13 +33,90 @@ class Proscan(SerialInstrument):
     def identify(self):
         return len(self.version()) == 3
 
+    def version(self):
+        '''Return 3-digit firmware version'''
+        return self.handshake('VERSION')
+
     def position(self):
+        '''Report the (x, y, z) coordinates of the stage
+
+        Returns
+        -------
+        position: list of int
+           x, y, z coordinates of current stage position
+        '''
         return list(map(int, self.handshake('P').split(',')))
 
-    def version(self):
-        return self.handshake('VERSION')
+    def set_origin(self):
+        '''Set the origin of the coordinate system
+
+        Returns
+        -------
+        success: bool
+             True: origin set to current position
+             False: communication error
+        '''
+        return self.expect('Z', '0')
+
+    def move_to(self, position, relative=False):
+        cmd = 'GR' if relative else 'G'
+        coordinates = ','.join(map(str, position))
+        return self.expect(f'{cmd},{coordinates}', 'R')
+
+    def move_to_origin(self):
+        return self.expect('M', 'R')
+
+    @pyqtProperty(float)
+    def resolution(self):
+        return float(self.handshake('RES,X'))
+
+    @resolution.setter
+    def resolution(self, value):
+        '''Get and set the resolution for the stage in micrometers'''
+        self.send(f'RES,s,{value}')
+
+    @pyqtProperty(int)
+    def speed(self):
+        return int(self.handshake('SMS'))
+
+    @speed.setter
+    def speed(self, value):
+        return self.expect(f'SMS,{value}', '0')
     
-    def read_lines(self, query):
+    @pyqtProperty(int)
+    def acceleration(self):
+        return int(self.handshake('SAS'))
+
+    @acceleration.setter
+    def acceleration(self, value):
+        '''Maximum acceleration of stage in range [1, 100]'''
+        return self.expect(f'SAS,{value}', '0')
+
+    @pyqtProperty(int)
+    def scurve(self):
+        return int(self.handshake('SCS'))
+
+    @scurve.setter
+    def scurve(self, value):
+        return self.expect(f'SCS,{value}', '0')
+
+    def stop(self):
+        '''Stop stage motion'''
+        return self.expect('I', 'R')
+    
+    def description(self):
+        '''Description of Proscan hardware'''
+        return self._read_lines('?')
+
+    def stage(self):
+        '''Description of stage'''
+        return self._read_lines('STAGE')
+
+    def focus(self):
+        '''Description of focus system'''
+        return self._read_lines('FOCUS')
+
+    def _read_lines(self, query):
         self.send(query)
         response = []
         while True:
@@ -48,12 +125,3 @@ class Proscan(SerialInstrument):
             if 'END' in this:
                 break
         return response
-
-    def describe(self):
-        return self.read_lines('?')
-
-    def stage(self):
-        return self.read_lines('STAGE')
-
-    def focus(self):
-        return self.read_lines('FOCUS')
