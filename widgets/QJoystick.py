5a5f5b1872e689e0e866fdf615da025e53c4678d
davidgrier <david.grier@nyu.edu>
2021-08-21 20:29:39 -0400

    streamline

--- widgets/QJoystick.py
+++ widgets/QJoystick.py
@@ -7,12 +7,12 @@ import logging
 
 logging.basicConfig()
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.WARNING)
+logger.setLevel(logging.DEBUG)
 
 
 class QJoystick(QWidget):
 
-    joystickChanged = pyqtSignal(object)
+    positionChanged = pyqtSignal(object)
 
     def __init__(self, *args,
                  fullscale=None,
@@ -20,25 +20,17 @@ class QJoystick(QWidget):
         super().__init__(*args, **kwargs)
         self.sizePolicy().setHeightForWidth(True)
         self.padding = 0.2
-        self.knobFraction = 0.3
-        self.joystickPosition = QPointF(0, 0)
+        self.knobSize = 0.3
         self.fullscale = fullscale or 1.
+        self.tolerance = 0.05
+        self.position = QPointF(0, 0)
         self._values = np.zeros(2)
         self.active = False
-
-    @pyqtProperty(float)
-    def fullscale(self):
-        return self._fullscale
-
-    @fullscale.setter
-    def fullscale(self, value):
-        self._fullscale = value
-        self.tolerance = 0.1 * value
 
     def resizeEvent(self, event):
         self.radius = min(self.size().width(), self.size().height()) / 2
         self.radius *= (1. - self.padding)
-        self.limit = (1. - self.knobFraction) * self.radius
+        self.limit = (1. - self.knobSize) * self.radius
         
     def paintEvent(self, event):
         if self.isEnabled():
@@ -49,59 +41,60 @@ class QJoystick(QWidget):
         painter.setRenderHint(QPainter.Antialiasing)
         painter.setPen(pen)
         painter.setBrush(bg)
-        painter.drawEllipse(self._ring())
+        painter.drawEllipse(self._limitRect())
         painter.setBrush(knob)
-        painter.drawEllipse(self._knob())
+        painter.drawEllipse(self._knobRect())
 
-    def _ring(self):
+    def _limitRect(self):
         rect = np.array([-1, -1, 2, 2]) * self.radius
         return QRectF(*rect).translated(self._center())
 
-    def _knob(self):
-        size = self.radius * self.knobFraction
+    def _knobRect(self):
+        size = self.radius * self.knobSize
         rect = np.array([-1, -1, 2, 2]) * size
-        pos = self.knobPosition if self.active else self._center()
+        pos = self.position if self.active else self._center()
         return QRectF(*rect).translated(pos)
 
     def _center(self):
         return QPointF(self.width()/2, self.height()/2)
 
-    def _boundJoystick(self, point):
+    def _limited(self, point):
         limitLine = QLineF(self._center(), point)
         if (limitLine.length() > self.limit):
             limitLine.setLength(self.limit)
         return limitLine.p2()
 
-    def fractions(self):
+    def mousePressEvent(self, ev):
+        self.active = self._knobRect().contains(ev.pos())
+        return super().mousePressEvent(ev)
+
+    def mouseReleaseEvent(self, event):
+        self.active = False
+        self.position = QPointF(0, 0)
+        self.update()
+        self.emitSignal()
+
+    def mouseMoveEvent(self, event):
+        self.position = self._limited(event.pos())
+        self.update()
+        self.emitSignal()
+
+    def _fractions(self):
         if self.active:
-            displacement = QLineF(self._center(), self.knobPosition)
+            displacement = QLineF(self._center(), self.position)
             fx = min(displacement.dx() / self.limit, 1.)
             fy = -min(displacement.dy() / self.limit, 1.)
         else:
             fx, fy = 0., 0.
         return np.array([fx, fy])
-
-    def mousePressEvent(self, ev):
-        self.active = self._knob().contains(ev.pos())
-        return super().mousePressEvent(ev)
-
-    def mouseReleaseEvent(self, event):
-        self.active = False
-        self.knobPosition = QPointF(0, 0)
-        self.update()
-        self.emitSignal()
-
-    def mouseMoveEvent(self, event):
-        self.knobPosition = self._boundJoystick(event.pos())
-        self.update()
-        self.emitSignal()
-
+    
     def emitSignal(self):
-        values = self.fullscale * self.fractions()
+        values = self._fractions()
         if np.allclose(values, self._values, self.tolerance):
             return
         self._values = values
-        self.joystickChanged.emit(values)
+        values *= self.fullscale
+        self.positionChanged.emit(values)
         logger.debug('{:.2f} {:.2f}'.format(*values))
 
 
