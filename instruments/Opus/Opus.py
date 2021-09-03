a3ea959d44c84ec88b98ca011223bf2840bdd9bb
claire0418 <64419346+claire0418@users.noreply.github.com>
2021-09-03 15:58:15 -0400

    Add files via upload

---
+++ instruments/Opus/Opus.py
@@ -0,0 +1,103 @@
+from PyQt5.QtCore import pyqtProperty
+from QInstrument.lib import SerialInstrument
+import numpy as np
+class opus(SerialInstrument):
+    '''Quantum Opus 570nm Laser
+
+    .....
+
+    Inherits
+    --------
+    SerialInstrument
+
+    Properties
+    ----------
+    portName: str
+        Name of the serial port to which the laser is attached
+
+    Methods
+    -------
+    get_power(): float
+         Power of laser output [mW]
+    set_power(float):
+         Set Power [mW]
+    '''
+
+    settings = dict(baudRate=SerialInstrument.Baud19200,
+                    dataBits=SerialInstrument.Data8,
+                    stopBits=SerialInstrument.OneStop,
+                    parity=SerialInstrument.NoParity,
+                    flowControl=SerialInstrument.NoFlowControl,
+                    eol='\r')
+    def __init__(self, portName=None, **kwargs):
+        super().__init__(portName, **self.settings, **kwargs)
+        self._status = false
+
+    def identify(self):
+        return 'MPC-D-1.0.07A' in self.handshake('VERSION?')
+    
+    def Property(pstr, dtype=float):      
+        def getter(self):
+            return self.get_value(f'{pstr}?', dtype=dtype)
+        def setter(self, value):
+            value = int(value) if (dtype == bool) else dtype(value)
+            self.send(f'{pstr}{value}')
+        return pyqtProperty(dtype, getter, setter)
+    
+    @pyqtProperty(float)
+    def keyswitch(self):
+        return float(self.handshake('STATUS?'))
+    
+    @pyqtProperty(bool)
+    def status(self):
+        return self._status
+    
+    @status.setter
+    def status(self, value):
+        if value == self._status:
+            return
+        if value == OFF:
+            self.send('OFF')
+            self._status = OFF
+        else:
+            self.send('ON')
+            self._status = ON
+            
+    @pyqtProperty(float)
+    def power(self):
+        return self.handshake('POWER?')
+        
+    @power.setter
+    def power(self, value):
+    '''Sets power (mW)'''
+        self.send('POWER={value}')
+        
+    @pyqtProperty(float)
+    def current(self)
+        return self.handshake('CURRENT?')
+        
+    @current.setter
+    def current(self, value):
+    '''Sets current as percentage of maximum'''
+        self.send('CURRENT={value}')
+        
+    #@pyqtProperty(float)
+    #def startPower(self)
+    #'''Get default start-up power (mW)'''
+        #return self.handshake('STPOW?')
+    
+    #@startPower.setter
+    #def startPower(self, value)
+    #'''Set default start-up power (mW)'''
+        #self.send('STPOW={VALUE}')
+        
+            
+    def get_lastemp(self):
+        self.handshake('LASTEMP?')
+        
+    def get_psutemp(self):
+        self.handshake('PSUTEMP?')
+        
+    def get_timers(self)
+    '''Get the timers of the laser and PSU'''
+        self.handshake('TIMERS?')
