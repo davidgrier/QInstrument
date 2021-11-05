9f324586aa8765d76f65e8863519824234034f8d
claire0418 <64419346+claire0418@users.noreply.github.com>
2021-11-05 16:22:12 -0400

    Create QOpus.py

---
+++ instruments/Opus/QOpus.py
@@ -0,0 +1,53 @@
+from QInstrument.lib import QInstrumentInterface
+from QInstrument.instruments.Opus.Opus import Opus
+from PyQt5.QtWidgets import QWidget
+from PyQt5.QtCore import pyqtSlot
+
+
+
+class QOpusWidget(QWidget):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args,
+                         uiFile='OpusWidget.ui',
+                         deviceClass=Opus
+                         **kwargs)
+        self.connectSignals()
+
+    def connectSignals(self):
+        self.ui.PowerDial.valueChanged.connect(self.updatePower)
+        self.ui.Power.editingFinished.connect(self.updatePowerDial)
+        self.ui.PowerDial.valueChanged.connect(self.uncheck)
+        self.ui.SendPower.clicked.connect(self.check)
+        self.device.get_power.connect(self.updateActualPower)
+	
+
+    @pyqtSlot(int)
+    def updatePower(self, value):
+        self.ui.Power.setValue(value)
+
+    @pyqtSlot()
+    def check(self):
+        self.ui.sentCheck.setChecked(True)
+
+    @pyqtSlot()
+    def uncheck(self):
+        self.ui.sentCheck.setChecked(False)
+
+    @pyqtSlot()
+    def updatePowerDial(self):
+        value = self.ui.Power.value()
+        self.ui.PowerDial.setValue(int(value))
+
+def main():
+    from PyQt5.QtWidgets import QApplication
+    import sys
+
+    app = QApplication(sys.argv)
+    widget = QOpusWidget()
+    widget.show()
+    sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    main()
