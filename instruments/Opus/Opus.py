6935e132b383e8e4405bf51e39a51b35baefae28
claire0418 <64419346+claire0418@users.noreply.github.com>
2021-11-10 11:58:12 -0500

    Update Opus.py

--- instruments/Opus/Opus.py
+++ instruments/Opus/Opus.py
@@ -81,15 +81,6 @@ class Opus(SerialInstrument):
         '''Sets power (mW)'''
         self.expect(f'POWER={value}', '')
         
-    def test(self):
-        power = self.handshake('POWER?')
-        numeric_filter = filter(str.isdigit, power)
-        p = float((int("".join(numeric_filter))/10))
-        if p == 0.0:
-                return 'OFF'
-        if p != 0.0:
-                return 'ON'
-        
     def current(self):
         return self.handshake('CURRENT?')
         
