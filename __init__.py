41a510766aef2d3ca9ee7301b92f75b2847ee004
davidgrier <david.grier@nyu.edu>
2022-05-31 20:26:45 -0400

    updating class names

--- __init__.py
+++ __init__.py
@@ -1,6 +1,10 @@
-from .instruments import (DS345, SR830, QDS345, QSR830, Proscan, QProscan,
-                          PDUS210, QPDUS210)
+from .instruments import (QDS345, QDS345Widget,
+                          QSR830, QSR830Widget,
+                          QProscan, QProscanWidget,
+                          QPDUS210, QPDUS210Widget)
 
 
-__all__ = ['DS345', 'SR830', 'QDS345', 'QSR830',
-           'Proscan', 'QProscan', 'PDUS210', 'QPDUS210']
+__all__ = ['QDS345', 'QDS345Widget',
+           'QSR830', 'QSR830Widget',
+           'QProscan', 'QProscanWidget',
+           'QPDUS210', 'QPDUS210Widget']
