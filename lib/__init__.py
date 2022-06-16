adb752f90d577a87a0316293168566416d877301
davidgrier <david.grier@nyu.edu>
2022-06-16 08:21:24 -0400

    support for placeholder interfaces

--- lib/__init__.py
+++ lib/__init__.py
@@ -1,10 +1,11 @@
 from .QInstrumentWidget import QInstrumentWidget
 from .QSerialInstrument import QSerialInstrument
+from .QFakeInstrument import QFakeInstrument
 from .QThreadedInstrumentWidget import QThreadedInstrumentWidget
 from .threadedInstrument import threadedInstrument
 from .Configure import Configure
 
 
-__all__ = ['QSerialInstrument', 'QInstrumentWidget',
+__all__ = ['QSerialInstrument', 'QFakeInstrument', 'QInstrumentWidget',
            'QThreadedInstrumentWidget', 'threadedInstrument',
            'Configure']
