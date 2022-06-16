67ab2cd3514666997782090e69616240513ba54a
davidgrier <david.grier@nyu.edu>
2022-06-16 10:39:22 -0400

    fix relative imports

--- instruments/SR830/__init__.py
+++ instruments/SR830/__init__.py
@@ -1,5 +1,6 @@
 from .QSR830 import QSR830
+from .QFakeSR830 import QFakeSR830
 from .QSR830Widget import QSR830Widget
 
 
-__all__ = ['QSR830', 'QSR830Widget']
+__all__ = ['QSR830', 'QFakeSR830', 'QSR830Widget']
