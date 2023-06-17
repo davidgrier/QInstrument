63c492978265a755a300eada5eacf012711cd91b
David Grier <david.grier@nyu.edu>
2023-06-17 17:45:53 -0400

    suppress arrow keys in spin box

--- widgets/QRotaryEncoderSpinBox.py
+++ widgets/QRotaryEncoderSpinBox.py
@@ -1,9 +1,22 @@
 from PyQt5 import uic
 from PyQt5.QtWidgets import QWidget
-from PyQt5.QtCore import pyqtSlot
+from PyQt5.QtCore import (QObject, QEvent, pyqtSlot)
+from PyQt5.Qt import Qt
 from matplotlib.colors import (to_rgb, to_hex)
 import numpy as np
 from pathlib import Path
+
+
+class SuppressArrowKeys(QObject):
+
+    def __init__(self, parent):
+        super().__init__(parent)
+
+    def eventFilter(self, object, event):
+        if event.type() == QEvent.KeyPress:
+            if event.key() in [Qt.Key_Up, Qt.Key_Down]:
+                return True
+        return False
 
 
 class QRotaryEncoderSpinBox(QWidget):
@@ -73,6 +86,8 @@ class QRotaryEncoderSpinBox(QWidget):
     def __init__(self, *args, colors=None, **kwargs):
         super().__init__(*args, **kwargs)
         self.ui = self._loadUi()
+        self.filter = SuppressArrowKeys(self)
+        self.ui.value.installEventFilter(self.filter)
         self._inheritMethods()
         self.setColors(colors or ('white', 'red'))
 
@@ -102,23 +117,20 @@ class QRotaryEncoderSpinBox(QWidget):
     def setColors(self, colors):
         self._colors = colors
         if colors is None:
-            self.valueChanged.disconnect(self._updateAppearance)
+            self.valueChanged.disconnect(self._setColor)
         else:
             self._c1 = np.array(to_rgb(colors[0]))
             self._c2 = np.array(to_rgb(colors[1]))
-            self.valueChanged.connect(self._updateAppearance)
+            self.valueChanged.connect(self._setColor)
 
-    def _setBackgroundColor(self, color):
+    @pyqtSlot(float)
+    def _setColor(self, value):
+        f = (value - self.minimum())/(self.maximum() - self.minimum())
+        color = to_hex((1-f)*self._c1 + f*self._c2)
         style = (f'QDoubleSpinBox {{'
                  f' background-color: {color};'
                  f' selection-background-color: {color}; }}')
         self.ui.value.setStyleSheet(style)
-
-    @pyqtSlot(float)
-    def _updateAppearance(self, value):
-        f = (value - self.minimum())/(self.maximum() - self.minimum())
-        color = to_hex((1-f)*self._c1 + f*self._c2)
-        self._setBackgroundColor(color)
 
 
 def example():
