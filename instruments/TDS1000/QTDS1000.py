6f813a8cbb14b7d3c48f39f845ba4ca9cf538f99
David Grier <david.grier@nyu.edu>
2024-02-23 17:23:45 -0500

    download data from scope

--- instruments/TDS1000/QTDS1000.py
+++ instruments/TDS1000/QTDS1000.py
@@ -7,8 +7,8 @@ class QTDS1000(QSerialInstrument):
                 dataBits=QSerialInstrument.Data8,
                 stopBits=QSerialInstrument.OneStop,
                 parity=QSerialInstrument.NoParity,
-                flowControl=QSerialInstrument.NoFlowControl,
-                eol=b'\r')
+                flowControl=QSerialInstrument.HardwareControl,
+                eol=b'\n')
 
     def __init__(self, portname=None, **kwargs):
         args = self.comm | kwargs
@@ -20,14 +20,37 @@ class QTDS1000(QSerialInstrument):
 
 def example():
     from PyQt5.QtCore import QCoreApplication
+    from struct import unpack
+    import numpy as np
+    import matplotlib.pyplot as plt
 
-    QCoreApplication([])
-    scope = QTDS1000().find()
+    app = QCoreApplication([])
+    scope = QTDS1000(timeout=500).find()
+    print(scope.portName())
     print(scope.handshake('*IDN?'))
 
-    print(scope.handshake('SELECT:CH1 ON'))
-    # scope.transmit('ACQUIRE:MODE SAMPLE')
-    # scope.transmit('ACQUIRE:STOPAFTER SEQUENCE')
+    scope.transmit('DATA:SOURCE CH1')
+    print(scope.handshake('DATA:SOURCE?'))
+    scope.transmit('DATA:WIDTH 1')
+    scope.transmit('DATA:ENC RPB')
+    print(scope.handshake('CH1:COUPLING?'))
+    ymult = scope.get_value('WFMPRE:YMULT?')
+    yzero = scope.get_value('WFMPRE:YZERO?')
+    yoff = scope.get_value('WFMPRE:YOFF?')
+    xincr = scope.get_value('WFMPRE:XINCR?')
+    xdelay = scope.get_value('HOR:POS?')
+    scope.transmit('CURVE?')
+    data = scope.receive(raw=True)
+
+    headerlen = 2 + int(data[1])
+    adc = data[headerlen:-1]
+    adc = np.array(unpack(f'{len(adc)}B', adc))
+    signal = (adc - yoff) * ymult + yzero
+    time = np.arange(0, (xincr * len(signal)), xincr)
+    time -= xincr * len(signal) / 2 - xdelay
+
+    plt.plot(time, signal)
+    plt.show()
 
 
 if __name__ == '__main__':
